6f26940e16deadfd09c6486c9a78206e65cd71d4
Hersh0359 <97804483+Hersh0359@users.noreply.github.com>
2024-02-02 20:30:55 -0500

    cleaned up Launcher a bit

--- src/main/java/frc/robot/subsystems/Launcher.java
+++ src/main/java/frc/robot/subsystems/Launcher.java
@@ -13,7 +13,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Launcher extends SubsystemBase{
     
     private CANSparkMax m_Roller1, m_Roller2, m_leftPivot, m_rightPivot;
-    private SparkPIDController roller1PID, roller2PID;
+    private SparkPIDController roller1PID, pivotPID;
     private DutyCycleEncoder pivotEncoder;
 
     
@@ -24,9 +24,12 @@ public class Launcher extends SubsystemBase{
         m_Roller1 = new CANSparkMax(53, MotorType.kBrushless);
         m_Roller2 = new CANSparkMax(54, MotorType.kBrushless);
 
-        roller1PID = m_Roller1.getPIDController();
-        roller2PID = m_Roller2.getPIDController();
+        
 
+        roller1PID = m_Roller1.getPIDController();
+        pivotPID = m_leftPivot.getPIDController();
+
+        
         pivotEncoder = new DutyCycleEncoder(0);
 
         configMotors();
@@ -34,32 +37,35 @@ public class Launcher extends SubsystemBase{
 
     public void launchWithVolts() {
         m_Roller1.setVoltage(6);
-        m_Roller2.setVoltage(6);
+       
     }
 
     public void stopLauncher() {
         m_Roller1.setVoltage(0);
-        m_Roller2.setVoltage(0);
+        
     }
 
     public void pivotUp() {
         m_leftPivot.setVoltage(6);
-        m_rightPivot.setVoltage(6);
+       
     }
 
     public void pivotDown() {
         m_leftPivot.setVoltage(-6);
-        m_rightPivot.setVoltage(-6);
-    }
+           }
 
     public void pivotStop() {
         m_leftPivot.setVoltage(0);
-        m_rightPivot.setVoltage(0);
+        
     }
 
-    public void setLauncherSetpoint(double setpoint) {
+    public void setLauncherVelocity(double setpoint) {
         roller1PID.setReference(setpoint, ControlType.kVelocity);
-        roller2PID.setReference(setpoint, ControlType.kVelocity);
+        
+    }
+
+    public double getAbsoluteMeasurement() {
+        return pivotEncoder.getAbsolutePosition();
     }
 
 
@@ -67,33 +73,37 @@ public class Launcher extends SubsystemBase{
         m_leftPivot.restoreFactoryDefaults();
         m_leftPivot.clearFaults();
         m_leftPivot.setIdleMode(IdleMode.kBrake);
-        m_leftPivot.burnFlash();
+        
 
         m_rightPivot.restoreFactoryDefaults();
         m_rightPivot.clearFaults();
         m_rightPivot.setIdleMode(IdleMode.kBrake);
         m_rightPivot.setInverted(true);
+        m_rightPivot.follow(m_leftPivot);
         m_rightPivot.burnFlash();
+        m_leftPivot.burnFlash();
+        
 
         m_Roller1.restoreFactoryDefaults();
         m_Roller1.clearFaults();
         m_Roller1.setIdleMode(IdleMode.kCoast);
-        m_Roller1.burnFlash();
 
         m_Roller2.restoreFactoryDefaults();
         m_Roller2.clearFaults();
         m_Roller2.setIdleMode(IdleMode.kCoast);
         m_Roller2.setInverted(true);
-        m_Roller2.burnFlash();
 
+        m_Roller2.follow(m_Roller1);
+        m_Roller2.burnFlash();
+        m_Roller1.burnFlash();
         
         roller1PID.setP(0.01);
         roller1PID.setI(0);
-        roller1PID.setD(0);
+        roller1PID.setD(0); 
 
-        roller2PID.setI(0.01);
-        roller2PID.setI(0);
-        roller2PID.setI(0);
+        // roller2PID.setI(0.01);
+        // roller2PID.setI(0);
+        // roller2PID.setI(0);
 
     }
 }
