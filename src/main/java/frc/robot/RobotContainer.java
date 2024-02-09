d60c5bde687880651703a6afbd17c72bbe73a1e0
walpolerobotics <roborebels1153@gmail.com>
2024-02-08 21:20:52 -0500

    Add in Mailbox

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Mailbox;
 import frc.robot.subsystems.Swerve;
 //import frc.robot.Constants.OperatorConstants;
 //import frc.robot.commands.Autos;
@@ -38,9 +39,8 @@ public class RobotContainer {
 
     private final Swerve s_Swerve = new Swerve();
     //private final Elevator elevator = new Elevator();
-    private final Collector collector = new Collector() {
-        
-    };
+    private final Collector collector = new Collector();
+    private final Mailbox mailbox = new Mailbox();
 
     private final int translationAxis = XboxController.Axis.kLeftY.value;
     private final int strafeAxis = XboxController.Axis.kLeftX.value;
@@ -96,11 +96,15 @@ public class RobotContainer {
         driveB.whileTrue(s_Swerve.sysIdQuasistatic(Direction.kReverse));
         driveX.whileTrue(s_Swerve.sysIdQuasistatic(Direction.kForward));
 
-        leftBumper.onTrue(new InstantCommand(() -> collector.intake()));
-        leftBumper.onFalse(new InstantCommand(() -> collector.collectorStop()));
+        //leftBumper.onTrue(new InstantCommand(() -> collector.intake()));
+        //leftBumper.onFalse(new InstantCommand(() -> collector.collectorStop()));
+        leftBumper.onTrue(new InstantCommand(() -> mailbox.sendToLauncher()));
+        leftBumper.onFalse(new InstantCommand(() -> mailbox.stop()));
 
-        rightBumper.onTrue(new InstantCommand(() -> collector.outtake()));
-        rightBumper.onFalse(new InstantCommand(() -> collector.collectorStop()));
+        //rightBumper.onTrue(new InstantCommand(() -> collector.outtake()));
+        //rightBumper.onFalse(new InstantCommand(() -> collector.collectorStop()));
+        rightBumper.onTrue(new InstantCommand(() -> mailbox.sendToIntake()));
+        rightBumper.onFalse(new InstantCommand(() -> mailbox.stop()));
 
         opY.onTrue(new InstantCommand(() -> collector.pivotUp()));
         opY.onFalse(new InstantCommand(() -> collector.pivotStop()));
